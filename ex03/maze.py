9ca77468b1b1ae72166aab42c35919c2fc51bf04
Azuma <C0A2110686@edu.teu.ac.jp>
2022-06-28 14:38:25 +0900

    練習45

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,9 +1,26 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    canvas.pack()
+
+    tori = tk.PhotoImage(file="fig/5.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
+
+    key = ""
+    root.bind
+
     root.mainloop()
-    #root.geometry("1500x900")
-    #entry = tk.Entry(root, )
+   
