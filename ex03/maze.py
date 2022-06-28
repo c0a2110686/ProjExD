41b5c9a1aa902d59ec5dcf491d1230d8dd9e15fa
Azuma <C0A2110686@edu.teu.ac.jp>
2022-06-28 14:20:08 +0900

    練習１

---
+++ ex03/maze.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    root.mainloop()
+    #root.geometry("1500x900")
+    #entry = tk.Entry(root, )
