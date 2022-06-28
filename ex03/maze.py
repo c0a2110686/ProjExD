299b0362329499128d515640b192b9e10d30c6ff
Azuma <C0A2110686@edu.teu.ac.jp>
2022-06-28 17:03:20 +0900

    追加1

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,62 +1,86 @@
 import tkinter as tk
 import maze_maker as mm
+import random
 
 def key_down(event):
-    global key
-    key = event.keysym
+  global key
+  key = event.keysym
+  #print(f"{key}キーが押されました")
 
 def key_up(event):
-    global key
-    key = ""
+ global key
+ key = ""
 
 def main_proc():
-    global cx, cy, mx, my
-    delta = { #キー:押されているキーkey/値: 移動幅リスト[x,y]
-        #""     : [0, 0],
-        "Up"   : [0, -1],
-        "Down" : [0, +1],
-        "Left" : [-1, 0],
-        "Right": [+1, 0],
-    }
-    try:
-        if maze_bg[mx+delta[key][1]][mx+delta[key][0]] == 0: #もし移動先が床なら
-            my, mx = my+delta[key][1], mx+delta[key][0]
-    except:
-        pass
+  global cx, cy, mx, my
 
-    #if key == "Up"    and maze_bg[my-1][mx] == 0 : my -= 1
-    #if key == "Down"  and maze_bg[my+1][mx] == 0 : my += 1
-    #if key == "Left"  and maze_bg[my][mx-1] == 0 : mx -= 1
-    #if key == "Right" and maze_bg[my][mx+1] == 0 : mx -= 1
-    cx, cy = mx*100+50, my*100+50
-    #cx, cy = cx+delta[key][0], cy+delta[key][1]
-    canvas.coords("tori", cx, cy)
-    root.after(100, main_proc)
-    
+  d = {  # キー：押されているキーkey/値: 移動幅リスト[x,y]
+    #"": [0, 0],
+    "Up":[0, -1],
+    "Down": [0, +1],
+    "Left": [-1, 0],
+    "Right": [+1, 0],
+      }
+  
+  try:
+    if maze_bg[ my+d[key][1]][mx+d[key][0]] == 0: # もし移動先が床ならば
+      my, mx =  my+d[key][1], mx+d[key][0]
+  except:
+    pass
 
+
+  cx, cy = mx*100+50, my*100+50
+  #cx, cy = cx+d[key][0], cy+d[key][1]
+  canvas.coords("tori", cx, cy)
+  root.after(100, main_proc)
+
+def add_enemy():
+    global cx, cy, dx, dy, teki
+    teki1 = tk.PhotoImage(file="fig/43012274426004b86abe4d47172c8ee8.png")
+    dx = random.randint(0, 13)
+    dy = random.randint(0, 13)
+    cx, cy = 100*dx, 100*dy
+    canvas.create_image(cx, cy, image=teki, tag="teki1")
+    canvas.coords("teki1", cx, cy)
+    root.after(200, add_enemy)
 
 if __name__ == "__main__":
-    #global key
-    root = tk.Tk()
-    root.title("迷えるこうかとん")
-    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
-    canvas.pack()
+  global key
+  root = tk.Tk()
+  root.title("pakupakumeiro")
+  
+  canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+  canvas.pack()
 
-    maze_bg = mm.make_maze(15, 9) #1:壁１/0:床を表す二次元リスト
-    mm.show_maze(canvas, maze_bg) #canvasにmaze_bgを描く
-    #print(maze_bg)
+  maze_bg = mm.make_maze(15, 9) # 1:壁 0:床 を表す二次元リスト
+  #print(maze_bg)
+  mm.show_maze(canvas, maze_bg)# 
 
-    tori = tk.PhotoImage(file="fig/5.png")
-    mx, my = 1, 1
-    cx, cy = mx*100+50, my*100+50
-    canvas.create_image(cx, cy, image=tori, tag="tori")
+  pakuman = tk.PhotoImage(file="fig/b312acaa0397b6d20574f475e35f5ffc.png")
+  pakuman = pakuman.zoom(5)
+  pakuman = pakuman.subsample(32)
+  mx, my = 1, 1
+  cx, cy = mx*100+50, my*100+50
+  #cx, cy = 300, 400
+  canvas.create_image(cx, cy, image=pakuman, tag="tori")
 
-    key = ""
-    
-    root.bind("<KeyPress>", key_down)
-    root.bind("<KeyRelease>", key_up)
+  dx = random.randint(0, 13)
+  dy = random.randint(0, 13)
+  teki = tk.PhotoImage(file="fig/43012274426004b86abe4d47172c8ee8.png")
+  teki = teki.zoom(5)
+  teki = teki.subsample(32)
+  cx, cy = 100*dx, 100*dy
+  canvas.create_image(cx, cy, image = teki, tag="teki1")
 
-    main_proc()
+  key = ""
 
-    root.mainloop()
-   
+  root.bind("<KeyPress>", key_down)
+  root.bind("<KeyRelease>", key_up)
+
+  root.after(0, main_proc)
+  root.after(0, add_enemy)
+
+  #main_proc()
+
+  
+  root.mainloop()
