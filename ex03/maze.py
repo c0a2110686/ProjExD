ad546f71f72ffec8366fd61f226d7de5a7332dcd
Azuma <C0A2110686@edu.teu.ac.jp>
2022-06-28 15:44:00 +0900

    練習1011

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,15 +10,26 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy
-    delta = { #キー:押されているキーkey/値: 移動幅リスト[x,y]
-        ""     : [0, 0],
-        "Up"   : [0, -20],
-        "Down" : [0, +20],
-        "Left" : [-20, 0],
-        "Right": [+20, 0],
-    }
-    cx, cy = cx+delta[key][0], cy+delta[key][1]
+    global cx, cy, mx, my
+    #delta = { #キー:押されているキーkey/値: 移動幅リスト[x,y]
+    #    ""     : [0, 0],
+    #    "Up"   : [0, -1],
+    #    "Down" : [0, +1],
+    #    "Left" : [-1, 0],
+    #    "Right": [+1, 0],
+    #}
+    #try:
+    #    if maze_bg[mx+delta[key][0]][mx+delta[key][0]] == 0: #もし移動先が床なら
+    #        my, mx = my+delta[key][1], mx+delta[key][0]
+    #except:
+    #    pass
+
+    if key == "Up"    and maze_bg[my-1][mx] == 0 : my -= 1
+    if key == "Down"  and maze_bg[my+1][mx] == 0 : my += 1
+    if key == "Left"  and maze_bg[my][mx-1] == 0 : mx -= 1
+    if key == "Right" and maze_bg[my][mx+1] == 0 : mx -= 1
+    cx, cy = mx*100+50, my*100+50
+    #cx, cy = cx+delta[key][0], cy+delta[key][1]
     canvas.coords("tori", cx, cy)
     root.after(100, main_proc)
     
@@ -36,7 +47,8 @@ if __name__ == "__main__":
     #print(maze_bg)
 
     tori = tk.PhotoImage(file="fig/5.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
