deaa4974fa64e17b62cb7e20599e93e17d583c3d
Azuma <C0A2110686@edu.teu.ac.jp>
2022-06-28 15:03:18 +0900

    練習67

--- ex03/maze.py
+++ ex03/maze.py
@@ -8,8 +8,23 @@ def key_up(event):
     global key
     key = ""
 
+def main_proc():
+    global cx, cy
+    delta = { #キー:押されているキーkey/値: 移動幅リスト[x,y]
+        ""     : [0, 0],
+        "Up"   : [0, -20],
+        "Down" : [0, +20],
+        "Left" : [-20, 0],
+        "Right": [+20, 0],
+    }
+    cx, cy = cx+delta[key][0], cy+delta[key][1]
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)
+    
+
 
 if __name__ == "__main__":
+    #global key
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
@@ -20,7 +35,10 @@ if __name__ == "__main__":
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
-    root.bind
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+
+    main_proc()
 
     root.mainloop()
    
