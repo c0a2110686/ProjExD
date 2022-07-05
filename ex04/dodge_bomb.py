e75826502450980df8bebed9060bbfdd8401b76f
Azuma <C0A2110686@edu.teu.ac.jp>
2022-07-05 14:34:06 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,13 +7,19 @@ def main():
     screen_sfc = pg.display.set_mode((1600, 900))  #1600x900の画面Surfaceを生成する
     screen_rct = screen_sfc.get_rect()             #Rect
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg")     #Surface 画像を挿入
-    bgimg_rct = bgimg_sfc.get_rect()               #こうかとんの場所を設定
+    bgimg_rct = bgimg_sfc.get_rect()               
     screen_sfc.blit(bgimg_sfc, bgimg_rct)          #screenSrfaceにbgimg_sfcをbgimg_rctの位置に貼り付ける
 
+    #練習3
+    kkimg_sfc = pg.image.load("fig/3.png")               #Surface
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) #Surface
+    kkimg_rct = kkimg_sfc.get_rect()                     #Rect
+    kkimg_rct.center = 900, 400                          #900x400の位置にこうかとんの場所を設定
     
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         #練習2
         for event in pg.event.get():
